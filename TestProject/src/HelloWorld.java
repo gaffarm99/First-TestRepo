0d1e6ec96f876f662504d508ed46acad995e690a
badal1443 <badal1443@gmail.com>
2014-05-14 02:32:50 -0700

    First time commiting from eclipse

---
+++ TestProject/src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	
+	public String ConverttoUpperCase(String val){
+		return val.toUpperCase();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld hello = new HelloWorld();
+		hello.ConverttoUpperCase("hello world!");
+
+	}
+
+}
